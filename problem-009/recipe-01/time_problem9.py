9a6117e70ed963518489951e01cc5b4dbf0db148
hexu <hexu_1985@sina.com>
2024-01-25 10:22:52 +0800

    update problem9

---
+++ problem-009/recipe-01/time_problem9.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+
+the_sum = 1000
+
+tic = time.time()
+for a in range(1, the_sum):
+    for b in range(a, the_sum-a):
+        c = the_sum - a - b
+        if c < a or c < b:
+            continue
+        if a**2 + b**2 == c**2:
+            toc = time.time()
+            print("{}**2 + {}**2 = {}**2".format(a, b, c))
+            print("ans:", a*b*c)
+            print("use time:", toc-tic, "sec")
+            sys.exit(1)
+
+
---
+++ problem-009/recipe-02/problem9.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+the_sum = 1000
+
+for a in range(1, the_sum//2):
+    for b in range(a, the_sum-a):
+        c = the_sum - a - b
+        if c < a or c < b:
+            break
+        if a**2 + b**2 == c**2:
+            print("{}**2 + {}**2 = {}**2".format(a, b, c))
+            print("ans:", a*b*c)
+            sys.exit(1)
+
+
